4907312f278e85996ed48acd7910675f639df83d
nmihiranga <mihiranganayana@gmail.com>
2024-03-27 04:25:04 +0530

    add to cart

--- src/test/java/LumaTest.java
+++ src/test/java/LumaTest.java
@@ -35,9 +35,9 @@ public class LumaTest {
     void scroll() throws InterruptedException {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         js.executeScript("window.scrollBy(0,-2200)", "");
-        Thread.sleep(4000);
+        Thread.sleep(3000);
     }
 
     //create account
@@ -56,7 +56,7 @@ public class LumaTest {
     }
 
     //sign in
-    @Test(priority = 2, enabled = false)
+    @Test(priority = 2)
     void signIn() throws InterruptedException {
         driver.findElement(By.linkText("Sign In")).click();
         driver.findElement(By.id("email")).sendKeys("test1@testmail.com");
@@ -65,19 +65,38 @@ public class LumaTest {
         driver.findElement(By.xpath("//*[@id=\"send2\"]/span")).click();
     }
 
+    //personal details add
+    @Test(priority = 3, enabled = false)
+    void detailsAdd() throws InterruptedException {
+        driver.findElement(By.xpath("//header/div[1]/div[1]/ul[1]/li[2]/span[1]/button[1]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.linkText("My Account")).click();
+        driver.findElement(By.linkText("Address Book")).click();
+        driver.findElement(By.id("telephone")).sendKeys("732185509");
+        driver.findElement(By.id("street_1")).sendKeys("Street 1");
+        driver.findElement(By.id("city")).sendKeys("City 1");
+        driver.findElement(By.id("region_id")).click();
+        driver.findElement(By.xpath("//option[contains(text(),'Alaska')]")).click();
+        driver.findElement(By.id("zip")).sendKeys("21000");
+        driver.findElement(By.id("country")).click();
+        driver.findElement(By.xpath("//option[contains(text(),'Albania')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//body/div[2]/main[1]/div[2]/div[1]/form[1]/div[1]/div[1]/button[1]")).click();
+    }
+
     //search
-    @Test(priority = 3)
+    @Test(priority = 4)
     void search() throws InterruptedException {
         WebElement m = driver.findElement(By.id("search"));
         m.sendKeys("Men tops");
         m.sendKeys(Keys.ENTER);
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,450)", "");
     }
 
     //add to cart
-    @Test(priority = 4)
+    @Test(priority = 5)
     void addToCart(){
         driver.findElement(By.id("option-label-size-143-item-168")).click();
         driver.findElement(By.id("option-label-color-93-item-57")).click();
@@ -85,18 +104,23 @@ public class LumaTest {
         driver.findElement(By.cssSelector("body.page-products.page-with-filter.catalogsearch-result-index.page-layout-2columns-left:nth-child(2) div.page-wrapper:nth-child(5) main.page-main div.columns:nth-child(4) div.column.main:nth-child(1) div.search.results:nth-child(6) div.products.wrapper.grid.products-grid:nth-child(3) ol.products.list.items.product-items li.item.product.product-item:nth-child(1) div.product-item-info div.product.details.product-item-details:nth-child(2) div.product-item-inner div.product.actions.product-item-actions div.actions-primary form:nth-child(1) > button.action.tocart.primary:nth-child(4)")).click();
     }
 
-
-    //menus
-
-    //add to cart
-    //filters
     //payout
+    @Test(priority = 6, enabled = false)
+    void payout(){
 
-    @Test
-    void logoToHome(){
-        //driver.findElement(By.linkText("https://magento.softwaretestingboard.com/")).click();
     }
 
+
+    //menus
+    //filters
+
+    //logout
+    @Test
+    void logout(){
+        //
+    }
+
+    //browser quit
     @AfterTest
     void tearDown(){
         //driver.quit();
